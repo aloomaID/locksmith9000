0591661b3bad37184e4b658622642257fdaab5ca
jjsalam007 <abdussalamjamal007@gmail.com>
2025-04-26 17:14:22 +0200

    Initial commit

--- fixfix.py
+++ fixfix.py
@@ -1,48 +1,72 @@
-c=0
-search_categories = open("/var/www/html/americanlocksmithsservice/duplicatescript1/staticfile/uscities - Sheet1.csv", "r", encoding="utf-8").readlines()
-for credentials in search_categories:
-    credential= credentials.strip().split(',')
-    sub_ciy_repl= credential[0].title().replace('ï»¿', '')
-    sub_ciy= credential[0].replace(' ', '-').replace(',', '').replace('.', '').replace('ï»¿', '').lower()
-    sub_ciy= str(sub_ciy.encode("ascii", "ignore")).replace("b'", '').replace("'", '').replace('b"', '').replace('"', '')
-    short_code = credential[2].upper().replace('ï»¿', '')
-    state_repl= credential[3].title().replace('ï»¿', '')
-    state= credential[3].replace(' ', '-').replace(',', '').replace('.', '').replace('ï»¿', '').lower()
-    state= str(state.encode("ascii", "ignore")).replace("b'", '').replace("'", '').replace('b"', '').replace('"', '')
-    population= int(credential[8])
-    zip_codes= credential[15]
-    if 'Puerto Rico'.replace(' ', '-').lower() == state:
-        continue
-    elif 'Virgin Islands'.replace(' ', '-').lower() == state:
-        continue
-    elif 'Washington, D.C.'.replace(' ', '-').lower() == state:
-        continue
-    elif 'US Armed Forces Pacific'.replace(' ', '-').lower() == state:
-        continue
-    elif 'American Samoa'.replace(' ', '-').lower() == state:
-        continue
-    elif 'Guam'.replace(' ', '-').lower() == state:
-        continue
-    elif 'Palau'.replace(' ', '-').lower() == state:
-        continue
-    elif 'Federated States of Micronesia'.replace(' ', '-').lower() == state:
-        continue
-    elif 'Northern Mariana Islands'.replace(' ', '-').lower() == state:
-        continue
-    elif 'Marshall Islands'.replace(' ', '-').lower() == state:
-        continue
-    elif 'US Armed Forces Europe'.replace(' ', '-').lower() == state:
-        continue
-    else:
-        pass
+import os
+import shutil
+from bs4 import BeautifulSoup
+import random
 
-    if population < 1000:
-        continue
-    c +=1
-    parent_dir2 = f"/var/www/html/locksmith9000/lockandkey/{state}/{sub_ciy}/index.html"
-    search_categories = open(parent_dir2, "r", encoding="utf8").read()
-    op= search_categories.replace("﻿", '').replace('aloomaid3', f'{sub_ciy_repl} {short_code}')
-    
-    fp = open(parent_dir2, "w", encoding='utf-8-sig')
+preloader='''<link href="static/img/favicon.ico" rel="icon">
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(101414766, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/101414766" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+</head>
+'''
+for d in os.listdir('lockandkey'):
+    try:
+        for c in os.listdir(f'lockandkey/{d}'):
+            try:
+                for b in os.listdir(f'lockandkey/{d}/{c}'):
+                    try:
+                        search_categories = open(f'lockandkey/{d}/{c}/{b}', "r", encoding="utf8").read()
+                        op= search_categories.replace("﻿", '')
+                        op= op.replace('</head>', preloader.replace('"static/img', '"../../static/img'))
+                        fp = open(f'lockandkey/{d}/{c}/{b}', "w", encoding='utf-8-sig')
+                        fp.writelines(op)
+                        fp.close()
+                    except:
+                        pass
+            except:
+                pass
+    except:
+        pass
+for d in os.listdir('lockandkey'):
+    try:
+        search_categories = open(f'lockandkey/{d}/index.html', "r", encoding="utf8").read()
+        op= search_categories.replace("﻿", '')
+        
+        op= op.replace('</head>', preloader.replace('"static/img', '"../static/img'))
+        fp = open(f'lockandkey/{d}/index.html', "w", encoding='utf-8-sig')
+        fp.writelines(op)
+        fp.close()
+    except:
+        pass
+try:
+    search_categories = open(f'lockandkey/index.html', "r", encoding="utf8").read()
+    op= search_categories.replace("﻿", '')
+    op= op.replace('</head>', preloader)
+    fp = open(f'lockandkey/index.html', "w", encoding='utf-8-sig')
     fp.writelines(op)
     fp.close()
+except:
+    pass
+try:
+    search_categories = open(f'index.html', "r", encoding="utf8").read()
+    op= search_categories.replace("﻿", '')
+    op= op.replace('</head>', preloader)
+    fp = open(f'index.html', "w", encoding='utf-8-sig')
+    fp.writelines(op)
+    fp.close()
+except:
+    pass
